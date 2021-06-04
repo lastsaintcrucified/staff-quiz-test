4055de2fa29aeb06930da6fb4f9d86d43984d0ca
unknown <lastsaintcrucified@gmail.com>
2021-06-05 00:56:01 +0600

    registration done

--- components/Register.js
+++ components/Register.js
@@ -1,25 +1,89 @@
+import Link from "next/link";
+import { useState, useContext } from "react";
+import { AppContext } from "./AppContext";
 import Styles from "../styles/register.module.css";
 
 const Register = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+    phone: "",
+  });
+  const context = useContext(AppContext);
+  const handleChange = (e) => {
+    e.preventDefault();
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+    if (data.password !== data.password_confirmation) {
+      return alert("password must match!");
+    }
+
+    fetch("https://backend.imentalhealth.net/api/register/student", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((raw) => {
+        context.setToken(raw.data.token);
+      });
+  };
+  console.log(context.token);
   return (
     <>
       <div className={Styles.main}>
         <h1>Let's Start</h1>
-        <form className={Styles.form}>
+        <form className={Styles.form} onSubmit={handleSubmit}>
           <label>Name</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={data.name}
+          />
           <label>Email</label>
-          <input type="email" name="email" />
+          <input
+            type="text"
+            name="email"
+            onChange={handleChange}
+            value={data.email}
+          />
           <label>Password</label>
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={data.password}
+          />
           <label>Confirm-Password</label>
-          <input type="password" name="con-password" />
+          <input
+            type="password"
+            name="password_confirmation"
+            onChange={handleChange}
+            value={data.password_confirmation}
+          />
           <label>Phone</label>
-          <input type="number" name="phone" />
+          <input
+            type="text"
+            name="phone"
+            onChange={handleChange}
+            value={data.phone}
+          />
+          <button type="submit" className={Styles.btn}>
+            Register
+          </button>
         </form>
-        <button className={Styles.btn}>Register</button>
       </div>
     </>
   );
 };
+
 export default Register;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { useState } from "react";
+import { AppContext } from "../components/AppContext";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [token, setToken] = useState("");
+  const value = { token, setToken };
+  return (
+    <AppContext.Provider value={value}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
